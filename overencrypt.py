83a09646fb8a55c2b9e2a46bc7ed60ae680f726e
Nick Doiron <ndoiron@mapmeld.com>
2016-12-10 21:28:09 -0500

    test

--- overencrypt.py
+++ overencrypt.py
@@ -8,9 +8,11 @@ import argparse
 
 # nginx tools
 import nginxparser
+DEFAULT_NGINX = '/etc/nginx/sites-enabled/default.conf'
 
 # apache tools
-import apache_conf_parser
+import ApacheConfParser from apache_conf_parser
+DEFAULT_VHOST = '/etc/apache2/sites-available/000-default.conf'
 
 # take in Nginx or Apache path
 parser = argparse.ArgumentParser(description="Upgrade security on your LetsEncrypt configuration on the server")
@@ -20,7 +22,13 @@ args = parser.parse_args(argv[1:])
 
 if args.nginx is not None:
     print('NGINX: ' + args.nginx)
+    madeit = nginxparser.load(open(args.nginx))
+    print(madeit)
+
 elif args.apache is not None:
     print('APACHE: ' + args.apache)
+    vhost = ApacheConfParser(args.apache)
+    print(vhost)
+
 else:
     print('no server config!!')
