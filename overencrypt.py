7b0515211a28812b774d12960edc1086f7128b29
Nick Doiron <ndoiron@mapmeld.com>
2016-12-10 21:08:24 -0500

    start getting config details

--- overencrypt.py
+++ overencrypt.py
@@ -1,3 +1,26 @@
 # overencrypt.py
+# open source
+# pip install -r requirements.txt
 
-# nginx and apache?
+# command line tools
+from sys import argv
+import argparse
+
+# nginx tools
+import nginxparser
+
+# apache tools
+import apache_conf_parser
+
+# take in Nginx or Apache path
+parser = argparse.ArgumentParser(description="Upgrade security on your LetsEncrypt configuration on the server")
+parser.add_argument('--nginx', required=False)
+parser.add_argument('--apache', required=False)
+args = parser.parse_args(argv[1:])
+
+if args.nginx is not None:
+    print('NGINX: ' + args.nginx)
+elif args.apache is not None:
+    print('APACHE: ' + args.apache)
+else:
+    print('no server config!!')
